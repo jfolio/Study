48d08431b8fd914b0f13107ff04421148d9db645
jfolio <63335605+jfolio@users.noreply.github.com>
2020-05-11 01:44:28 +0900

    Add files via upload

--- JYAN/js/js.js
+++ JYAN/js/js.js
@@ -22,6 +22,23 @@ $(document).ready(function(){
 		$('html').animate({scrollTop:offset.top},400);
 	});
 
+	/* 1 */
+    $(window).scroll( function(){
+        /* 2 */
+        $('.skills').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* 3 */
+            if( bottom_of_window > bottom_of_object/1 ){
+							$('.pgb1 div').addClass('.skill1');
+							$('.pgb2 div').addClass('.skill2');
+							$('.pgb3 div').addClass('.skill3');
+							$('.pgb4 div').addClass('.skill4');
+              $('.pgb5 div').addClass('.skill5');
+            }
+        });
+    });
+
 	$('.stc_contact').click(function(){
 		var offset = $('#contact').offset();
 		$('html').animate({scrollTop:offset.top},400);
